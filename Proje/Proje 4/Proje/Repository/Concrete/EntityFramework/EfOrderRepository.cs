9df54dcd8eee42695da4ce3172e18e974a3c2571
burhan154 <59660549+burhan154@users.noreply.github.com>
2021-12-16 23:53:16 +0300

    Add files via upload

---
+++ Proje/Proje 4/Proje/Repository/Concrete/EntityFramework/EfOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using Proje.Entity;
+using Proje.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Repository.Concrete.EntityFramework
+{
+    public class EfOrderRepository:EfGenericRepository<Order>,IOrderRepository
+    {
+        public EfOrderRepository(EduraContext context):base(context)
+        {
+
+        }
+    }
+}
---
+++ Proje/Proje 4/Proje/Repository/Concrete/EntityFramework/EfProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using Proje.Entity;
+using Proje.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Proje.Repository.Concrete.EntityFramework
+{
+    public class EfProductRepository : EfGenericRepository<Product>, IProductRepository
+    {
+
+        public EfProductRepository(EduraContext context):base(context)
+        {
+
+        }
+        public EduraContext EduraContext
+        {
+            get { return context as EduraContext; }
+        }
+        public List<Product> GetTop5Products()
+        {
+            return EduraContext.Products.OrderByDescending(x => x.ProductId).Take(5).ToList();
+        }
+    }
+}
