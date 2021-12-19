df453fb77e7236012a13e627f306fc7238ecc7b5
burhan154 <59660549+burhan154@users.noreply.github.com>
2021-12-19 23:43:21 +0300

    sepet

---
+++ Proje/Proje 7/Proje/Entity/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Entity
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        [Required]
+        public string CategoryName { get; set; }
+        public List<ProductCategory> ProductCategories { get; set; }
+    }
+}
---
+++ Proje/Proje 7/Proje/Entity/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Entity
+{
+    public class Image
+    {
+        public int ImageId { get; set; }
+        public string ImageName { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; } //navigation property
+
+
+    }
+}
