ba0eb39258d2d9d955aff83562bb6067ad26462c
burhan154 <59660549+burhan154@users.noreply.github.com>
2021-12-21 13:40:36 +0300

    Add files via upload

---
+++ Proje/Proje 8/Proje/Models/CatalogListModel.cs
@@ -0,0 +1,14 @@
+﻿using Proje.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Models
+{
+    public class CatalogListModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
