af23ebde4fdd467df38c3d680958327fdd08d1c3
burhan154 <59660549+burhan154@users.noreply.github.com>
2021-12-23 18:06:45 +0300

    Add files via upload

---
+++ Proje/Proje 10/Proje/Components/CategoryMenu.cs
@@ -0,0 +1,22 @@
+﻿using Proje.Repository.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Components
+{
+    public class Categories:ViewComponent
+    {
+        private ICategoryRepository repository;
+        public Categories(ICategoryRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.GetAllWithProductCount());
+        }
+    }
+}
