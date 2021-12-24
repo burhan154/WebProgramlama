5fc45f2a232261f9e7c096e16bd98c4ab6160560
burhan154 <59660549+burhan154@users.noreply.github.com>
2021-12-24 21:04:38 +0300

    Add files via upload

---
+++ Proje/Proje 11/Proje/Controllers/AccountController.cs
@@ -0,0 +1,109 @@
+﻿using Proje.IdentityCore;
+using Proje.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private UserManager<ApplicationUser> userManager;
+        private SignInManager<ApplicationUser> signInManager;
+
+        public AccountController(UserManager<ApplicationUser> _userManager, SignInManager<ApplicationUser> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel model,string returnUrl)
+        {//return url kullanıcının ilk başta gitmeye calışıp login olmadığı için gidemediği url
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(model.Email);
+
+                if (user!=null)
+                {
+                    await signInManager.SignOutAsync();
+                    var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(model.Email), "Email or Password is not correct!!!");
+            }
+            return View(model);
+        }
+        ///register
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Register(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model, string returnUrl)
+        {//return url kullanıcının ilk başta gitmeye calışıp login olmadığı için gidemediği url
+            if (ModelState.IsValid)
+            {
+
+                var user = new ApplicationUser
+                {
+
+                    Name = model.Name,
+                    Email = model.Email,
+                    NormalizedUserName= model.Name,
+                    NormalizedEmail = model.Email,
+                    UserName = model.Name,
+                    LastName=model.LastName
+                 
+                };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return Redirect(returnUrl ?? "/");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+
+        }
+    }
+}
---
+++ Proje/Proje 11/Proje/Models/LoginModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+    }
+}
