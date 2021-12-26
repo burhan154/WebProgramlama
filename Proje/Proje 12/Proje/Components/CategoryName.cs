8000d52d2f9209e6e40d89d11f856e15af631e69
burhan154 <59660549+burhan154@users.noreply.github.com>
2021-12-26 12:32:09 +0300

    Add files via upload

---
+++ Proje/Proje 12/Proje/Components/CategoryName.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proje.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje.Components
+{
+    public class CategoryName : ViewComponent
+    {
+        private ICategoryRepository repository;
+        public CategoryName(ICategoryRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(repository.GetAll().Take(4));
+        }
+    }
+}
